f85f61153d2401c90814a2e6553fceede1186a36
Howard Nguyen <info@howardnguyen.com>
2024-06-16 17:03:53 -0700

    update model URL to godaddy

--- app.py
+++ app.py
@@ -21,19 +21,26 @@ stacking_model = load_model(model_url)
 st.title("Cardiovascular Disease Prediction App by Howard Nguyen")
 st.write("Enter your parameters and click Predict to get the results.")
 
-# Input features
-age = st.slider("Enter your age:", 32, 81, 54)
-totchol = st.slider("Total Cholesterol:", 107, 696, 200)
-sysbp = st.slider("Systolic Blood Pressure:", 83, 295, 151)
-diabp = st.slider("Diastolic Blood Pressure:", 30, 150, 89)
-bmi = st.slider("BMI:", 14.43, 56.80, 26.77)
-cigpday = st.slider("Cigarettes Per Day:", 0, 90, 20)
-cursmoke = st.selectbox("Current Smoker:", [0, 1])
-glucose = st.slider("Glucose:", 39, 478, 117)
-diabetes = st.selectbox("Diabetes:", [0, 1])
-heartrate = st.slider("Heart Rate:", 37, 220, 91)
-prevap = st.selectbox("Prevalent Ap:", [0, 1])
-stroke = st.selectbox("Stroke:", [0, 1])
+# Create two columns for the layout
+col1, col2 = st.columns(2)
+
+with col1:
+    age = st.slider("Enter your age:", 32, 81, 54)
+    totchol = st.slider("Total Cholesterol:", 107, 696, 200)
+    sysbp = st.slider("Systolic Blood Pressure:", 83, 295, 151)
+    diabp = st.slider("Diastolic Blood Pressure:", 30, 150, 89)
+    bmi = st.slider("BMI:", 14.43, 56.80, 26.77)
+    cigpday = st.slider("Cigarettes Per Day:", 0, 90, 20)
+    cursmoke = st.selectbox("Current Smoker:", [0, 1])
+
+with col2:
+    glucose = st.slider("Glucose:", 39, 478, 117)
+    diabetes = st.selectbox("Diabetes:", [0, 1])
+    heartrate = st.slider("Heart Rate:", 37, 220, 91)
+    prevap = st.selectbox("Prevalent Ap:", [0, 1])
+    stroke = st.selectbox("Stroke:", [0, 1])
+    bpmeds = st.selectbox("On BP Meds:", [0, 1])
+    hyperten = st.selectbox("Hypertension:", [0, 1])
 
 # Constructing DataFrame for prediction
 input_data = pd.DataFrame({
@@ -48,7 +55,9 @@ input_data = pd.DataFrame({
     'DIABETES': [diabetes],
     'HEARTRTE': [heartrate],
     'PREVAP': [prevap],
-    'STROKE': [stroke]
+    'STROKE': [stroke],
+    'BPMEDS': [bpmeds],
+    'HYPERTEN': [hyperten]
 })
 
 st.write("### Input Parameters")
@@ -60,7 +69,7 @@ if st.button("Predict"):
         predictions = stacking_model.predict_proba(input_data)
         rf_pred = predictions[:, 1]
         
-        st.write(f"Random Forest Prediction: CVD with probability {rf_pred[0]:.2f}")
+        st.write(f"Stacking Model Prediction: CVD with probability {rf_pred[0]:.2f}")
         
     except Exception as e:
         st.error(f"Error making predictions: {e}")
